99865fe43e0191e31adb05c69c1be852defd60d4
Rakesh <rgoswami.dev@gmail.com>
2022-06-28 17:35:06 +0530

    2nd commit

--- server/index.js
+++ server/index.js
@@ -12,4 +12,11 @@ app.use(
     graphiql: process.env.NODE_ENV === "development",
   })
 );
+app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: process.env.NODE_ENV === "development",
+    })
+  );
 app.listen(port, console.log(`Server running on port ${port}`));
